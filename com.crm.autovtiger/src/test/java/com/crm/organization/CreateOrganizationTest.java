45e5e56d6964848290fdb1b5ac9fcc9ed8325159
Agnello12345 <agnello29117@gmail.com>
2022-08-15 11:18:32 +0530

    SCM push

--- com.crm.autovtiger/src/test/java/com/crm/organization/CreateOrganizationTest.java
+++ com.crm.autovtiger/src/test/java/com/crm/organization/CreateOrganizationTest.java
@@ -8,20 +8,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class CreateOrganizationTest 
 {
 	@Test
 	public void createOrganizationTest() throws Throwable
 	{
-		
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
-		FileInputStream fis = new FileInputStream("C:\\Users\\PC\\eclipse-workspace\\com.crm.autovtiger\\src\\test\\resources\\Data.properties");
+		FileInputStream fis = new FileInputStream("C:\\Users\\PC\\git\\genuinerepo\\com.crm.autovtiger\\src\\test\\resources\\Data.properties");
 	    Properties prop = new Properties();
 	    prop.load(fis);
 	    String url = prop.getProperty("url");
 	    System.out.println(url);
 	    driver.get(url);
 	    System.out.println("execution done");
+	    System.out.println("Execution in jenkins poll SCM mode is done");
 	
 	
 	}
