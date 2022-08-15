e8114eed6b64e1a59c929c25afeee7ca1768cfb1
Agnello12345 <agnello29117@gmail.com>
2022-08-15 11:45:23 +0530

    Added Choice Parameter

--- com.crm.autovtiger/src/test/java/com/crm/organization/CreateOrganizationTest.java
+++ com.crm.autovtiger/src/test/java/com/crm/organization/CreateOrganizationTest.java
@@ -17,12 +17,13 @@ public class CreateOrganizationTest
 	{
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
-		FileInputStream fis = new FileInputStream("C:\\Users\\PC\\git\\genuinerepo\\com.crm.autovtiger\\src\\test\\resources\\Data.properties");
-	    Properties prop = new Properties();
-	    prop.load(fis);
-	    String url = prop.getProperty("url");
-	    System.out.println(url);
-	    driver.get(url);
+//		FileInputStream fis = new FileInputStream("C:\\Users\\PC\\git\\genuinerepo\\com.crm.autovtiger\\src\\test\\resources\\Data.properties");
+//	    Properties prop = new Properties();
+//	    prop.load(fis);
+	    //String url = prop.getProperty("url");
+		String URL=System.getProperty("url");
+	    System.out.println(URL);
+	    driver.get(URL);
 	    System.out.println("execution done");
 	    System.out.println("Execution in jenkins poll SCM mode is done");
 	
